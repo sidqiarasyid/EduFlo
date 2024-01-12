78e944890dce93ab3caecabe5ff3c095f929d6ef
sidqi <sidqiarasyid31@gmail.com>
2024-01-12 13:49:47 +0700

    feat: [GA] Sign Up and Sign In page

--- lib/page/sign_up.dart
+++ lib/page/sign_up.dart
@@ -1,15 +1,14 @@
 import 'package:edu_flo/code_assets/assets.dart';
-import 'package:edu_flo/page/sign_up.dart';
 import 'package:flutter/material.dart';
 
-class SignInPage extends StatefulWidget {
-  const SignInPage({Key? key}) : super(key: key);
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({Key? key}) : super(key: key);
 
   @override
-  State<SignInPage> createState() => _SignInPageState();
+  State<SignUpPage> createState() => _SignUpPageState();
 }
 
-class _SignInPageState extends State<SignInPage> {
+class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,25 +20,17 @@ class _SignInPageState extends State<SignInPage> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text("Sign In", style: StyleText.title(null)),
+            Text("Sign up", style: StyleText.title(null)),
             SpaceWidget.height(context, 0.03),
-            Text(
-                "Let’s continue your recent progress and make it better then before",
-                style: StyleText.subtitle(null),
-                textAlign: TextAlign.center),
+            Text("Let’s begin our journey and step forward",
+                style: StyleText.subtitle(null)),
             SpaceWidget.height(context, 0.03),
             CustomWidget.formField(hintText: 'Email', isPass: false),
             SpaceWidget.height(context, 0.02),
             CustomWidget.formField(hintText: 'Password', isPass: true),
-            SpaceWidget.height(context, 0.01),
-            Row(
-              children: [
-                Expanded(child: Container()),
-                Text("Reset Password", style: StyleText.overline(null)),
-              ],
-            ),
             SpaceWidget.height(context, 0.05),
-            CustomWidget.primaryBtn(btnGoogle: false, txtDefault: 'Sign In'),
+            CustomWidget.primaryBtn(
+                btnGoogle: false, txtDefault: 'Let’s start'),
             SpaceWidget.height(context, 0.05),
             Row(
               children: [
@@ -58,16 +49,14 @@ class _SignInPageState extends State<SignInPage> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text("Don’t have an account?", style: StyleText.textSpan(null)),
+                Text("Already have an account?",
+                    style: StyleText.textSpan(null)),
                 SpaceWidget.width(context, 0.02),
                 GestureDetector(
                   onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const SignUpPage()));
+                    Navigator.pop(context);
                   },
-                  child: Text("Sign up",
+                  child: Text("Sign in",
                       style: StyleText.richText(const Color(0xFF1CADFF))),
                 ),
               ],
